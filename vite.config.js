6fd19724a4005b2da88306b2d6325f9bdbc65eca
Norskfan77 <williamhawkins_@hotmail.co.uk>
2025-02-20 11:48:54 +0000

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/norskfan77.github.io/",  // Set this to match your repo name
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://norsk-be-ny-production.up.railway.app"
+    base: "/", // Use "/" for user/organization pages
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/api": "https://norsk-be-ny-production.up.railway.app"
+        }
     }
-  }
 });
